2b30fdea48ea07f8dc70bdf952e1a6e55a6277af
constantine@aitrailblazer.com <constantine@aitrailblazer.com>
2024-09-22 22:45:00 -0700

    update

--- aitrailblazer.AppHost/Program.cs
+++ aitrailblazer.AppHost/Program.cs
@@ -1,7 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.aitrailblazer_ApiService>("apiservice");
-
+//
 builder.AddProject<Projects.aitrailblazer_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(apiService);
