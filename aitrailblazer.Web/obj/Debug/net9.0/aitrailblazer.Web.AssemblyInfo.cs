b51f1368d6a819b66b94d995dd698ad6086681e3
constantine@aitrailblazer.com <constantine@aitrailblazer.com>
2024-09-30 00:18:40 -0700

    upload

--- aitrailblazer.Web/obj/Debug/net9.0/aitrailblazer.Web.AssemblyInfo.cs
+++ aitrailblazer.Web/obj/Debug/net9.0/aitrailblazer.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aitrailblazer.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05040dab59705337b7d3d2d01a41a362a3549c59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+340e43c01bc5154d06dc11ed944a260386a10e62")]
 [assembly: System.Reflection.AssemblyProductAttribute("aitrailblazer.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aitrailblazer.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
