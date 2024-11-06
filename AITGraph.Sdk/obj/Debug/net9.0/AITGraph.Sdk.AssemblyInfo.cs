8a037ae28ffe64ec32b9fc35d105838898a6ed6d
constantine@aitrailblazer.com <constantine@aitrailblazer.com>
2024-11-06 11:02:07 -0800

    update

--- AITGraph.Sdk/obj/Debug/net9.0/AITGraph.Sdk.AssemblyInfo.cs
+++ AITGraph.Sdk/obj/Debug/net9.0/AITGraph.Sdk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AITGraph.Sdk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82a786cef5740bff7fa8c6c6ca3926e8725f9a00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+557e3d23489bb9eeff89baeaa68ec9fc8155ca42")]
 [assembly: System.Reflection.AssemblyProductAttribute("AITGraph.Sdk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AITGraph.Sdk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
