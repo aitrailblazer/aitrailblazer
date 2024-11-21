ad3d7eb75a4dea5a2233b20d3170ecde1b8662ab
constantine@aitrailblazer.com <constantine@aitrailblazer.com>
2024-11-21 13:08:14 -0800

    upload

--- CognitiveServices.Sdk/obj/Debug/net9.0/CognitiveServices.Sdk.AssemblyInfo.cs
+++ CognitiveServices.Sdk/obj/Debug/net9.0/CognitiveServices.Sdk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CognitiveServices.Sdk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b08e7b432e18be754eaf6c1a8ab2d34ee4188e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6770c5b8801a361b979c0f8e195511eea22f553")]
 [assembly: System.Reflection.AssemblyProductAttribute("CognitiveServices.Sdk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CognitiveServices.Sdk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
